2182672b2e2495880f1ad6c4906c0f016a0d1579
david-code-portfolio <ksendzulak.biz@gmail.com>
2026-02-19 01:28:17 +0100

    Checkboxes fixed

--- src/components/checkBoxFilter.jsx
+++ src/components/checkBoxFilter.jsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
 
 function checkBoxFilter({heading, filterOptions}) {
-
 
     return <div className="mt-4">
         <h3 className="text-non-active">{heading}</h3>
@@ -9,15 +9,22 @@ function checkBoxFilter({heading, filterOptions}) {
             {
                 filterOptions.map((option, index) => {
                     return <li key={index}>
-                        <input 
-                            type="checkbox" 
-                            name={option} 
-                            id={option}
-                            className="border-accent focus:ring-contast hidden"
-                        />
-                        <label htmlFor={option} className="w-fit flex gap-2 cursor-pointer hover:text-contrast duration-300 hover:border-contrast group/checkbox">
-                            <div className={`w-4 h-4 border-2 border-soft-grey rounded-sm self-center group-hover/checkbox:border-contrast duration-300`}></div>
-                            {option}
+                        <label 
+                            htmlFor={option} 
+                            className="w-fit flex gap-2 cursor-pointer hover:text-contrast duration-300 hover:border-contrast group/checkbox"
+                        >
+                            <input 
+                                type="checkbox" 
+                                name={option} 
+                                id={option}
+                                className="hidden peer"
+                            />
+                            <div className={`peer-checked:bg-accent peer-checked:border-0 w-4 h-4 items-center justify-center flex border-2 border-soft-grey rounded-sm self-center group-hover/checkbox:border-contrast duration-300`}>
+                                <FontAwesomeIcon icon={faCheck} className="text-[0.5rem] text-main"></FontAwesomeIcon>
+                            </div>
+                            <span className="peer-checked:text-accent peer-checked:font-medium">
+                                {option}
+                            </span>
                         </label>
                     </li>
                 })
